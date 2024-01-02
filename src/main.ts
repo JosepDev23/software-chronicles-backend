29e40a5b3b7f5d0af55d390ecc698a03d7c577de
JosepDev23 <josepdev23@gmail.com>
2024-01-02 19:43:43 +0100

    add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Software Chronicles API')
+    .setDescription('Documentation')
+    .setVersion('0.1')
+    .build()
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('api', app, swaggerDocument)
+
+  await app.listen(process.env.PORT || 3001)
 }
-bootstrap();
+
+bootstrap()
