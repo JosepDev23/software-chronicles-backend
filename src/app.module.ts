ce3afbb2d33214093a4d83f1bcc246519dba6620
JosepDev23 <josepdev23@gmail.com>
2024-01-02 19:58:26 +0100

    add database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.PROD_USER}:${process.env.PROD_PASSWORD}@${process.env.PROD_CLUSTER}.tmj5t2q.mongodb.net/${process.env.PROD_DATABASE}?retryWrites=true&w=majority`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
