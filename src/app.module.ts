02f68e0b5b92367f24808179a904e3bae9867885
JosepDev23 <josepdev23@gmail.com>
2024-01-02 21:01:51 +0100

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { UserModule } from './modules/user/user.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.PROD_USER}:${process.env.PROD_PASSWORD}@${process.env.PROD_CLUSTER}.tmj5t2q.mongodb.net/${process.env.PROD_DATABASE}?retryWrites=true&w=majority`,
     ),
